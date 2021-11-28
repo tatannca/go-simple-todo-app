3f6881b5dd1fa01ba0eaa1b51668a3100bec3714
tateno <gd28.c.tateno@gmail.com>
2021-11-28 15:56:11 +0900

    User crud

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.17
 
 require gopkg.in/go-ini/ini.v1 v1.64.0
 
-require github.com/stretchr/testify v1.7.0 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,16 +2,37 @@ package main
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/wd30gsrc/go-simple-todo-app/config"
+	"github.com/wd30gsrc/go-simple-todo-app/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+	// log.Println("test2")
 
-	log.Println("test2")
+	fmt.Println(models.Db)
+
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
+
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
