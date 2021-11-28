36d7373858d2f2105df6db588cfb620df5aaa532
tateno <gd28.c.tateno@gmail.com>
2021-11-28 22:37:40 +0900

    Todo crud

--- main.go
+++ main.go
@@ -16,23 +16,47 @@ func main() {
 	fmt.Println(models.Db)
 
 	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
 	// u.PassWord = "testtest"
 	// fmt.Println(u)
 
 	// u.CreateUser()
 
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
 
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// user, _ := models.GetUser(2)
+	// fmt.Println(user)
+	// user.CreateTodo("Frist Todo")
+
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("Third Todo")
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// user2, _ := models.GetUser(3)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	t, _ := models.GetTodo(3)
+	t.DeleteTodo()
 }
